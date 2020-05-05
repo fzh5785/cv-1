6f313f19de46b7ee42c1fd9095516c21b33a39c0
ISol <599304776@qq.com>
2020-05-05 16:14:56 +0800

    init2

--- src/main.js
+++ src/main.js
@@ -1,26 +1,25 @@
 let html = document.querySelector("#html");
 let style = document.querySelector("#style");
-let string = `
-/*你好，我叫冯子豪
- *接下来我演示一下我的前端功底
- *首先我要准备一个div
-*/
+let string = `/* 你好，我叫冯子豪
+ * 接下来我演示一下我的前端功底
+ * 首先我要准备一个div
+ **/
 #div1{
     border:1px solid red;
     width:200px;
     height:200px;
 }
-/*接下来我把div变成一个八卦图
- *注意看好了
- *首先，把div变成一个圆
+/* 接下来我把div变成一个八卦图
+ * 注意看好了
+ * 首先，把div变成一个圆
  **/
 #div1 {
     border-radius:50%;
     box-shadow:0 0 3px rgba(0,0,0,.5);
     border:none;
 }
-/*八卦是阴阳形成的
- *一黑一白
+/* 八卦是阴阳形成的
+ * 一黑一白
  **/
 #div1{
     background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
